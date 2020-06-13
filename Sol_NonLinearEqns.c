83aff1df8e112595481435124fe149d94ca6c1ae
MoonSunBeen <54530356+MoonSunBeen@users.noreply.github.com>
2020-06-14 06:42:46 +0900

    Update Sol_NonLinearEqns.c

--- Sol_NonLinearEqns.c
+++ Sol_NonLinearEqns.c
@@ -4,6 +4,19 @@
 #include "basicHeader.h"
 #include "UserDefine.h"
 #include "Sol_NonLinearEqns.h"
+#include "Linear_Algebra.h"
+
+void readFile(OUT float result[READ_ROW][READ_COLUMN]) {
+	//printf("%f \n", RR[0][0]);
+	FILE* myfile;
+	myfile = fopen("hw3_3b.dat", "r");
+	for (int i = 0; i < READ_ROW; i++) {
+		fscanf(myfile, "%f %f %f", &result[i][0], &result[i][1], &result[i][2]);
+	}
+
+	fclose(myfile);
+}
+
 
 void print_MethodOpt(OUT unsigned int *_METHOD) {
 
@@ -41,6 +54,14 @@ void RootFinding(IN int Method) {
 
 	case SECANT:
 		RootFinding_SECANT(initS0, initS1);
+		break;
+
+	case NEWTONRAPHON:
+		RootFinding_NEWTONRAPHON(initNR);
+		break;
+
+	case RECURSIVE_LMS:
+		Recursive_LMS(prex);
 		break;
 
 	default: 
@@ -92,7 +113,7 @@ float RootFinding_NEWTON(IN float initN) {
 
 	for ( ; seq < MAX_ITER; seq++) {
 		
-		if(seq > 1) curN = preN - Diff0_Func(preN) / Diff1_Func(preN);
+		curN = preN - Diff0_Func(preN) / Diff1_Func(preN);
 
 		Disp(NEWTON, seq, curN, Diff1_Func(curN), Diff0_Func(curN), 0.0, 0.0);
 
@@ -128,6 +149,92 @@ float RootFinding_SECANT(IN float initS0, IN float initS1) {
 	if (seq >= MAX_ITER) { State = ERR_ITER; Error_Message(State); }
 }
 
+float Recursive_LMS(OUT float out[READ_ROW]) {
+	readFile(RR);
+
+	//initalize and calulate usually used parameter
+	for (int j = 0; j < READ_COLUMN; j++) {
+		for (int k = 0; k < READ_COLUMN; k++) {
+			preP[j][k] = initP[j][k];
+			prex[j] = initx[j];
+		}
+	}
+	
+	float trans_H[READ_COLUMN][READ_COLUMN] = { 0, };
+	transpose_matrix(H_LMS, trans_H);
+
+	float inv_R[READ_COLUMN][READ_COLUMN] = { 0, };
+	matrix_inverse_3(R_LMS, inv_R);
+
+	float invR_transH[READ_COLUMN][READ_COLUMN] = { 0, };
+	matrix_mul(trans_H, inv_R, invR_transH);
+
+	float kf[READ_COLUMN][READ_COLUMN] = { 0, };
+
+	for (int i = 0; i < 100; i++) {
+		//calulate y
+		for (int w = 0; w < READ_COLUMN; w++) {
+			y[w] = sqrt(RR[i][w]) + minus_y[w];
+		}
+
+		float temp[READ_COLUMN][READ_COLUMN] = { 0, };
+
+		//Gramian matrix update
+		float invP[READ_COLUMN][READ_COLUMN] = { 0 };
+		matrix_mul(invR_transH, H_LMS, temp);
+		matrix_inverse_3(preP, invP);
+		matrix_add(temp, invP, invcurP);
+
+	
+		//calculation of the kalman gain
+		matrix_inverse_3(invcurP, curP);
+		matrix_mul(curP, invR_transH, kf);
+
+		//calculation of the residual sequence using sensor measurement vector y
+		float temp1[READ_COLUMN] = { 0 };
+		for (int i = 0; i < READ_COLUMN; i++) {
+			for (int k = 0; k < READ_COLUMN; k++) {
+				temp1[i] = temp1[i] + H_LMS[i][k] * prex[k];
+			}
+		}
+
+		for (int j = 0; j < READ_COLUMN; j++) {
+			y_tilda[j] = y[j] - temp1[j];
+		}
+
+
+		//update the LS solution
+		float temp2[READ_COLUMN] = { 0 };
+		for (int i = 0; i < READ_COLUMN; i++) {
+			for (int k = 0; k < READ_COLUMN; k++) {
+				temp2[i] = temp2[i] + kf[i][k] * y_tilda[k];
+			}
+		}
+
+		for (int l = 0; l < READ_COLUMN; l++) {
+			curx[l] = prex[l] + temp2[l];
+		}
+
+		//update value
+		for (int j = 0; j < READ_COLUMN; j++) {
+			prex[j] = curx[j];
+			for (int k = 0; k < READ_COLUMN; k++) {
+				preP[j][k] = curP[j][k];
+				
+			}
+		}
+
+		
+
+	}
+
+	for (int k = 0; k < READ_COLUMN; k++) {
+		printf("%f ", curx[k]);
+	}
+
+	printf("\n");
+}
+
 void Disp(IN int _Method, IN int seq, IN float disp1, IN float disp2, IN float disp3, IN float disp4, IN float disp5) {
 	
 	if (_Method == 1) {
@@ -153,12 +260,19 @@ void Disp(IN int _Method, IN int seq, IN float disp1, IN float disp2, IN float d
 		printflag = 1;
 		printf("%d        %f      %f      %f\n", seq, disp1, disp2, disp3);
 	}
-	
+
+	if (_Method == 4){
+		if (printflag == 0) printf("Iteration # |\n");
+		printflag = 1;
+		printf("%2d          |   %2f\n", seq, disp1);
+	}
 }
 
 void Error_Message(IN unsigned int _Errortype) {
 	if (_Errortype == 1) printf("Converge\n");
 	if (_Errortype == 2) printf("Over Iterate\n");
 	if (_Errortype == 3) printf("Diverge\n");
+	if (_Errortype == 4) printf("Order is too high\n");
+	if (_Errortype == 5) printf("There is No Solution\n");
 }
 #endif
